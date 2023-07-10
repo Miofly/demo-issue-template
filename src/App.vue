85231abdeca29648ad73557618ead285c3cf358a
wfly <953212389@qq.com>
2023-07-10 09:28:57 +0800

    feat: update feature

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { Button } from 'vant';
+import 'vant/lib/button/style/index';
 </script>
 
 <template>
-  <div id="app">
-    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-
-        <nav>
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
-        </nav>
-      </div>
-    </header>
-
-    <router-view />
+  <div>
+      <Button>11</Button>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite'
 import legacy from '@vitejs/plugin-legacy'
 import vue2 from '@vitejs/plugin-vue2'
 import vue2Jsx from '@vitejs/plugin-vue2-jsx'
+import Components from 'unplugin-vue-components/vite'
+import {
+    VantResolver,
+} from 'unplugin-vue-components/resolvers'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +18,11 @@ export default defineConfig({
         legacy({
             targets: ['ie >= 11'],
             additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+        }),
+        Components({
+            resolvers: [
+                VantResolver(),
+            ],
         })
     ],
     resolve: {
