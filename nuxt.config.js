205ad9e36d2186483ec22f9e34cc9097bcc42bb9
wfly <953212389@qq.com>
2023-07-24 23:24:43 +0800

    feat: update feature

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,22 @@
 import { defineNuxtConfig } from '@nuxt/bridge'
 
 export default defineNuxtConfig({
-  target: 'static',
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'pinia-example-nuxt',
+    title: 'demo-nuxt-bridge',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-
-  css: ['~/assets/main.css'],
-
   components: true,
-
-  buildModules: [
-    // set `disableVuex` to false if you need to use Vuex alongside Pinia
-    ['@pinia/nuxt', { disableVuex: true }],
-  ],
-
   build: {
-    transpile: [
-      // necessary for nuxt bridge
-      'pinia',
-    ],
-  },
-
-  bridge: {
-    vite: true
-    // nitro: true,
-    // capi: false,
-  },
+    // @ts-ignore
+    transpile: ['ofetch']
+  }
 })
