76ea9796fd9d079ba4f45cb219c352ce48ba4724
wfly <953212389@qq.com>
2023-07-30 22:04:32 +0800

    feat: update feature

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,6 @@
 		<!--<img width="100" height="100" src="/pwa-192.png" alt="">-->
 		<!--<img width="100" height="100" src="/img/pwa-512.png" alt="">-->
 		<div>default</div>
-		<slot />
+		<Nuxt />
 	</div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
+import IconText from '~/components/icon-text.vue';
+
 const test = ref('我的11身上');
 </script>
 
 <template>
 	<div>
-		<p>Hello!111</p>
-		<div>{{ test}}</div>
+		<div>{{ test }}</div>
 		<icon-text></icon-text>
-		<icon></icon>
 	</div>
 </template>
