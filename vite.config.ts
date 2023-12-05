b09de4be6ca0b4183b9c97aa94234995e2d41f3f
wfly <953212389@qq.com>
2023-12-05 11:07:00 +0800

    feat: update feature

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import Components from 'unplugin-vue-components/vite';
 import VueMacros from 'unplugin-vue-macros/vite';
-import { CompResolver } from 'wfly';
 
 // https://vitejs.dev/config/
 export default defineConfig({
